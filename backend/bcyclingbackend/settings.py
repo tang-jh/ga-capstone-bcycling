c2f9274c090e49054da24f0b05f8867e70b64eb4
junhao-tang <junhao.tang@outlook.com>
2021-11-17 23:47:08 +0800

    patched settings.py

--- backend/bcyclingbackend/settings.py
+++ backend/bcyclingbackend/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
 
     #Local Apps
     'friends.apps.FriendsConfig'
@@ -132,7 +132,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
