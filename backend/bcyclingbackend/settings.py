e768e613af12931dfbe026cec48330bc6dfeafbb
junhao-tang <junhao.tang@outlook.com>
2021-11-17 23:23:17 +0800

    updated deployment config

--- backend/bcyclingbackend/settings.py
+++ backend/bcyclingbackend/settings.py
@@ -137,3 +137,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
