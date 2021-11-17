fc0b5181136e38cca8bc26fd6f33204addcad19f
junhao-tang <junhao.tang@outlook.com>
2021-11-17 17:12:38 +0800

    updated prelim config for backend. to test deployment
    updated deployment config
    
    removed staticfiles from settings.py
    
    patched settings.py
    
    patched settings.py
    
    patched settings.py
    
    patched settings.py
    
    patched settings.py
    
    patched settings.py
    
    patched settings.py
    
    patched settings.py
    
    patched settings.py
    
    relocated Procfile
    
    relocated runtime.txt

---
+++ backend/friends/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.conf import settings
+
+
+# Create your models here.
+class Friend(models.Model):
+    f_id = models.AutoField(primary_key=True)
+    requester = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                  related_name="requester",
+                                  on_delete=models.CASCADE)
+    to_friend = models.ForeignKey(settings.AUTH_USER_MODEL,
+                                  related_name="friend",
+                                  on_delete=models.CASCADE)
+    accepted = models.DateTimeField(null=True, blank=True)
