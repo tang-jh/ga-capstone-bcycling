f8e0843691082e034460715093ac1e99f36c3b3e
junhao-tang <junhao.tang@outlook.com>
2021-11-18 00:14:44 +0800

    patched settings.py

--- backend/bcyclingbackend/settings.py
+++ backend/bcyclingbackend/settings.py
@@ -87,11 +87,11 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('LOCAL_DB_NAME'),
-        'USER': os.environ.get('LOCAL_DB_USER'),
-        'PASSWORD': os.environ.get('LOCAL_DB_PASSWORD'),
-        'HOST': os.environ.get('LOCAL_DB_HOST'),
-        'PORT': os.environ.get('LOCAL_DB_PORT')
+        'NAME': os.environ.get('LOCAL_DB_NAME', ''),
+        'USER': os.environ.get('LOCAL_DB_USER', ''),
+        'PASSWORD': os.environ.get('LOCAL_DB_PASSWORD', ''),
+        'HOST': os.environ.get('LOCAL_DB_HOST', ''),
+        'PORT': os.environ.get('LOCAL_DB_PORT', '')
     }
 }
 
