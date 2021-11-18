f812a1d135fd685247fc574ae5c3da81347a915a
junhao-tang <junhao.tang@outlook.com>
2021-11-18 09:44:40 +0800

    patched settings.py

--- backend/bcyclingbackend/settings.py
+++ backend/bcyclingbackend/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 import environ
 import os
+import django_heroku
 
 env = environ.Env()
 environ.Env.read_env()
@@ -132,9 +133,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR, 'staticfiles'
+STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -148,3 +151,5 @@ DATABASES['default'].update(db_from_env)
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
