787ed62bf0012d70a26be0b7b8df7a5b2f98d5ab
junhao-tang <junhao.tang@outlook.com>
2021-11-21 17:09:03 +0800

    added Broute and Comment model

--- backend/broutes/models.py
+++ backend/broutes/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
+class BRoute(models.Model):
+    r_id = models.BigAutoField(primary_key=True)
+    userFK = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, default='No title')
+    route = models.TextField()
+    distance = models.DecimalField(decimal_places=2)
+    difficulty_choices = [('EASY', 'Easy'), ('INTERMEDIATE', 'Intermediate'),
+                          ('DIFFICULT', 'Difficult')]
+    difficulty = models.CharField(max_length=15,
+                                  choices=difficulty_choices,
+                                  default='EASY')
+    description = models.TextField(max_length=500, blank=True)
+    created = models.DateTimeField(null=True, blank=True)
+    edited = models.DateTimeField(null=True, blank=True)
+    deleted = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'broutes'
+
+
+class Comment(models.Model):
+    c_id = models.BigAutoField(primary_key=True)
+    comment = models.TextField(max_length=500)
+    routeFK = models.ForeignKey(BRoute, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now=True)
+    deleted = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'comments'
